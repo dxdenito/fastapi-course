53f35691cd30928e99aa99eb81dcd418e44437ce
dxdenito <denixkaranjah@gmail.com>
2026-06-29 18:16:56 +0300

    feat: add health and info endpoint

--- app/main.py
+++ app/main.py
@@ -8,3 +8,20 @@ app = FastAPI(
 @app.get("/")
 def read_root():
     return {"message":"Welcome to Fastapi course"}
+
+@app.get("/info")
+def get_info():
+    info = {
+        "framework":"fastapi",
+        "python_version":"3.14.6",
+        "developer":"Denis Kibathi Karanja"
+    }
+
+    return info
+
+@app.get("/health")
+async def health_check():
+    health ={
+        "status":"ok"
+    }
+    return health
