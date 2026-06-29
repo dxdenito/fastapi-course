1ea6110ff8b4bfd73708d7b2b2580c506e9e6ffb
dxdenito <denixkaranjah@gmail.com>
2026-06-29 17:50:17 +0300

    feat: add project structure and first endpoint

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="FastAPI course",
+    description="Learning FastAPI via claude course"
+)
+
+app.get("/")
+def read_root():
+    return{"message":"Welcome to Fastapi course"}
